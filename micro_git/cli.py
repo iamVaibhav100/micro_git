1a89e7873ed8a6954d9a4c10f5c3d50cd774e026
Vaibhav Shukla <vaibhavshukla1967@gmail.com>
2024-05-30 12:26:33 +0530

    added hash function to store file and print its value upon calling

--- micro_git/cli.py
+++ micro_git/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 from . import data
 
 
@@ -17,6 +18,14 @@ def parse_args():
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)
 
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
+
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument('object')
+
     return parser.parse_args()
 
 
@@ -24,3 +33,12 @@ def init(args):
     data.init()
     print(f'Initialized an empty mgit repo in {os.getcwd()}/{data.GIT_DIR}')
 
+
+def hash_object(args):
+    with open(args.file, 'rb') as f:
+        print(data.hash_object(f.read()))
+
+
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object))
--- micro_git/data.py
+++ micro_git/data.py
@@ -1,7 +1,21 @@
 import os
+import hashlib
 
 GIT_DIR = '.mgit'
 
 
 def init():
     os.makedirs(GIT_DIR)
+    os.makedirs(f'{GIT_DIR}/objects')
+
+
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
+        out.write(data)
+    return oid
+
+
+def get_object(oid):
+    with open(f'{GIT_DIR}/objects/{oid}', 'rb') as f:
+        return f.read()
